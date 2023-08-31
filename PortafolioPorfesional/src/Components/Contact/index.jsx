b4566414b7c667d74a143cd64d54ff6a3dd445c4
Leslie-L <leslie.centeno@galileo.edu>
2023-08-31 13:56:47 -0600

    solving bugs

--- PortafolioPorfesional/src/Components/Contact/index.jsx
+++ PortafolioPorfesional/src/Components/Contact/index.jsx
@@ -9,23 +9,23 @@ function Contact() {
             <div className="flex flex-col justify-center items-center lg:flex-row lg:space-x-8">
                 <p className="font-montserrat text-md ml-4 lg:pt-20 lg:w-96 lg:self-start lg:text-xl">{contactInfo.Paragraph}</p>
                 <form className="w-[310px] h-[480px] flex flex-col justify-center shadow-xl rounded-lg p-3">
-                    <label htmlFor="name" >
+                    
                         <span className="font-beba font-semibold text-blue">{contactInfo.FORM[0]}</span>
                         <input className="w-full shadow-lg h-8 rounded-md" type="text" name="name" id="name" />
-                    </label>
-                    <label htmlFor="subject" >
+                    
+                    
                         <span className="font-beba font-semibold text-blue">{contactInfo.FORM[1]}</span>
                         <input className="w-full shadow-lg h-8 rounded-md" type="text" name="subject" id="subject" />
-                    </label>
-                    <label htmlFor="email" >
+                    
+                    
                         <span className="font-beba font-semibold text-blue">{contactInfo.FORM[2]}</span>
                         <input className="w-full shadow-lg h-8 rounded-md" type="email" name="email" id="email" />
-                    </label>
-                    <label htmlFor="">
+                    
+                    
                         <span className="font-beba font-semibold text-blue">{contactInfo.FORM[3]}</span>
                         <textarea className="w-full shadow-lg  rounded-md"  name="" id="" cols="30" rows="8"></textarea>
-                    </label>
-                    <button className="w-full h-8 rounded-md bg-orange font-bebas text-lg text-bold text-white hover:bg-pink">{contactInfo.BUTTON}</button>
+                    
+                    <button className="w-full h-8 rounded-md mt-2  bg-orange font-bebas text-lg text-bold text-white hover:bg-pink">{contactInfo.BUTTON}</button>
                 </form>
             </div>
             
